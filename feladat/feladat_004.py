81f5f9ca3c1fa53e20c736ae6ae281a5204271bc
szemandani <113088556+SZEMdani@users.noreply.github.com>
2023-10-06 12:56:21 +0200

    Add files via upload

---
+++ feladat/feladat_004.py
@@ -0,0 +1,6 @@
+idei_év = 2023
+felhasználó_kora = input('Hány éves vagy? ')
+print(f"Te most {felhasználó_kora} éves vagy.")
+felhasználó_kora = int(felhasználó_kora)
+születési_év = idei_év - felhasználó_kora
+print(f"Ekkor születtél: {születési_év}.")
---
+++ feladat/feladat_006.py
@@ -0,0 +1,9 @@
+# feladat_006
+# dk9 108-109 oldal
+
+gondolt_szám = 4
+tipp = input('Gondoltam egy számra. Tippeld meg! ')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print("Ügyes!")
+    print("Gratulálok.")
