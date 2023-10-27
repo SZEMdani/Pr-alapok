cb0e8fc65716869b2db772ea6422e32163b2cf26
szemandani <113088556+SZEMdani@users.noreply.github.com>
2023-10-27 12:12:50 +0200

    Add files via upload

---
+++ hazi_feladatok/haz1.py
@@ -0,0 +1,7 @@
+szam = int(input("Adj meg egy számot! "))
+if szam < 0:
+    print("A megadott szám negatív.")
+elif szam == 0:
+    print("A megadott szám nulla.")
+else:
+    print("A megadott szám pozitív.")
---
+++ hazi_feladatok/hazi4.py
@@ -0,0 +1,10 @@
+def koszont():
+    print("Üdv a fedélzeten!")
+    
+def koszont_nevvel(nev):
+    print("Szia " + nev + ", üdva fedélzeten!")
+    
+def koszont_ket_nevvel(nev1, nev2):
+    print("Szia " + nev1 + ' és ' + nev2 + ", üdv a fedélzeten!")
+    
+koszont_ket_nevvel("Nóra", "Ádám")
